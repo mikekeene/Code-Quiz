59fefbafeba91e350b59e27adc3d6896364bd496
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2021-11-26 20:38:34 -0600

    javascript

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,18 @@
 function initQuiz() {
-    
+  let timeRemaining=0;
+      const startButtonEl = document.getElementById("start-button");
+      const timeRemainingEl = document.getElementById("time-remaining");
+      const finalScoreEl = document.getElementById("final-score");
+      const numQuestions = questions.length;
+      const landingContainerEl = document.getElementById("landing-container");
+      const quizContainerEl = document.getElementById("quiz-container");
+      const finalContainerEl = document.getElementById("final-container");
+      const submitButtonEl = document.getElementById("submit-initials");
+      const highscoreButtonEl = document.getElementById("highscore-button");
+      const highscoreContainerEl = document.getElementById("highscore-container");
+      let highScores = [];
+      if (JSON.parse(localStorage.getItem('scores')) !== null) {
+          highScores = JSON.parse(localStorage.getItem("scores"));
+      }
+  
 }
